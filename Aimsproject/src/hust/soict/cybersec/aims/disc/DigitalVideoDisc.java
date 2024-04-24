1cc0bcb41020d7c4531d1d6cb4c287bb46ea198c
TapTap <phamtuananh8a1@gmail.com>
2024-04-24 17:11:53 +0700

    3.Classifier members and Instance member

--- Aimsproject/src/hust/soict/cybersec/aims/disc/DigitalVideoDisc.java
+++ Aimsproject/src/hust/soict/cybersec/aims/disc/DigitalVideoDisc.java
@@ -6,13 +6,13 @@ public class DigitalVideoDisc{
 	private String director;
 	private int length;
 	private float cost;
-//	private static int nbDigitalVideoDiscs = 0;
-//	private int id;
+	private static int nbDigitalVideoDiscs = 0;
+	private int id;
 	
 	public DigitalVideoDisc(String title, String category, float cost) {
 		super();
-//		this.nbDigitalVideoDiscs ++;
-//		this.id = this.nbDigitalVideoDiscs;
+		nbDigitalVideoDiscs ++;
+		this.id = nbDigitalVideoDiscs;
 		this.title = title;
 		this.category = category;
 		this.cost = cost;
@@ -20,8 +20,8 @@ public class DigitalVideoDisc{
 
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
 		super();
-//		this.nbDigitalVideoDiscs ++;
-//		this.id = this.nbDigitalVideoDiscs;
+		nbDigitalVideoDiscs ++;
+		this.id = nbDigitalVideoDiscs;
 		this.title = title;
 		this.category = category;
 		this.director = director;
@@ -46,9 +46,9 @@ public class DigitalVideoDisc{
 	public float getCost() {
 		return cost;
 	}
-//	public int getId() {
-//		return this.id;
-//	}
+	public int getId() {
+		return this.id;
+	}
 	
 	public void setTitle(String title) {
 		this.title = title;
