a3807008913726884e45e82b32a462dbfcde31b4
TapTap <phamtuananh8a1@gmail.com>
2024-04-24 16:36:27 +0700

    2.Applying method overloading

--- Aimsproject/src/hust/soict/cybersec/aims/cart/Cart.java
+++ Aimsproject/src/hust/soict/cybersec/aims/cart/Cart.java
@@ -22,6 +22,17 @@ public class Cart {
         }
     }
     
+    public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList ) {
+    	for(int i = 0; i < dvdList.length; i++) {
+    		this.addDigitalVideoDisc(dvdList[i]);
+    	}
+    }
+    
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+    	this.addDigitalVideoDisc(dvd1);
+    	this.addDigitalVideoDisc(dvd2);
+    }
+    
     public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
         for (int i = 0; i < qtyOrdered; i++) {
             if (itemsOrdered[i].equals(disc)) {
@@ -35,17 +46,6 @@ public class Cart {
             }
         }
         System.out.println("The disc was not found in the cart.");
-    }
- 
-    public void addDigitalVideoDisc(DigitalVideoDisc...dvdList ) {
-    	for(int i = 0; i < dvdList.length; i++) {
-    		this.addDigitalVideoDisc(dvdList[i]);
-    	}
-    }
-
-    public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
-    	this.addDigitalVideoDisc(dvd1);
-    	this.addDigitalVideoDisc(dvd2);
     }
     
     public float getTotalCost() {
@@ -66,9 +66,6 @@ public class Cart {
     	}
     	ans += ("Total cost: " + Float.toString(this.getTotalCost()));
     	return ans;
-    }
-    public void searchById(int id) {
-    	
     }
 }
 	
